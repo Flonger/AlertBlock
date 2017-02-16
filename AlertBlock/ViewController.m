0bad8aa9dbfb39baaf14d0a71fb4655ee805f694
Flonger <xue@flonger.com>
2017-02-16 22:15:43 +0800

    利用runtime实现alert代理只写一次

--- AlertBlock/ViewController.m
+++ AlertBlock/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
 
-@interface ViewController ()
+static void *MyAlertKey = "MyAlertKey";
+
+
+@interface ViewController ()<UIAlertViewDelegate>
 
 @end
 
@@ -17,12 +21,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 0, 100, 100);
+    btn.center = self.view.center;
+    [btn setBackgroundColor:[UIColor yellowColor]];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    
 }
 
+- (void)btnClick
+{
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"111111"
+                                                     message:@"222222"
+                                                    delegate:self
+                                           cancelButtonTitle:@"cancel"
+                                           otherButtonTitles:@"continue", nil];
+    void (^block)(NSInteger) = ^(NSInteger buttonIndex){
+        if (buttonIndex == 0) {
+            NSLog(@"cancel");
+        }else{
+            NSLog(@"continue");
+        }
+        
+    };
+    objc_setAssociatedObject(alert,
+                             MyAlertKey,
+                             block,
+                             OBJC_ASSOCIATION_COPY);
+    
+    [alert show];
+    
+    
+    
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    void (^block)(NSInteger) = objc_getAssociatedObject(alertView, MyAlertKey);
+    block(buttonIndex);
+
 }
 
 
